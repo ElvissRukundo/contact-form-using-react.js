0e749aab4683c88de87c69e0f625527b082721e0
Elvis Rukundo <rukundoelvis99@gmail.com>
2024-10-30 12:39:39 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ContactForm from './contactForm';
+
+const App = () => {
+  return (
+    <div>
+      <ContactForm />
+    </div>
+  );
+};
+
+export default App;
